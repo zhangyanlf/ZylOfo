bff06f820262e0566edc591e5611691d83caf92e
zhangyanlf <zhyl529@126.com>
2017-06-20 10:06:28 +0800

    添加pod 及侧滑菜单

--- ZylOfo/MenuController.swift
+++ ZylOfo/MenuController.swift
@@ -36,7 +36,7 @@ class MenuController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+/*
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
@@ -47,7 +47,7 @@ class MenuController: UITableViewController {
         return 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
--- ZylOfo/ViewController.swift
+++ ZylOfo/ViewController.swift
@@ -7,17 +7,26 @@
 //
 
 import UIKit
+import SWRevealViewController
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.titleView = UIImageView(image: #imageLiteral(resourceName: "ofoLogo"))
         self.navigationItem.leftBarButtonItem?.image = #imageLiteral(resourceName: "leftTopImage").withRenderingMode(.alwaysOriginal)
         self.navigationItem.rightBarButtonItem?.image = #imageLiteral(resourceName: "rightTopImage").withRenderingMode(.alwaysOriginal)
         
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        
+        if let revealVC = revealViewController() {
+            revealVC.rearViewRevealWidth = 288
+            navigationItem.leftBarButtonItem?.target = revealVC
+            navigationItem.leftBarButtonItem?.action = #selector(SWRevealViewController.revealToggle(_:))
+            view.addGestureRecognizer(revealVC.panGestureRecognizer())
+        }
         
     }
 
